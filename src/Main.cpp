c66fe7d6f7831abbea8fbe28fe6930ddd7090ca2
Shadax-stack <58188800+Shadax-stack@users.noreply.github.com>
2022-08-26 01:01:04 -0700

    Transform data to better distribution

--- src/Main.cpp
+++ src/Main.cpp
@@ -100,12 +100,10 @@ public:
     void Train(const std::vector<Sample>& training_set) {
         int num_factors = training_set.back().factors.size();
         // for each id, create a binary classifier that determines the sample as "part of class x" or "not part of class x"
+        std::vector<Sample> onevall_set = training_set;
         for (int i = 0; i < num_factors; i++) {
-            std::vector<Sample> onevall_set;
-            for (const Sample& s : training_set) {
-                Sample binclass = s;
-                binclass.label = (binclass.label == i ? 1 : -1);
-                onevall_set.push_back(binclass);
+            for (int j = 0; j < training_set.size(); j++) {
+                onevall_set[j].label = (training_set[j].label == i ? 1 : -1);
             }
 
             T binclassifier;
@@ -133,24 +131,21 @@ private:
     std::vector<T> classifiers;
 };
 
-int main() {
-    CSVReader reader("iris.csv");
-
+float program() {
+    MultiClassifier<Perceptron> classifier;
     std::vector<Sample> data_set;
 
-    MultiClassifier<Perceptron> classifier;
-
+    CSVReader reader("iris.csv");
     for (CSVRow& row : reader) {
         Sample sample;
-        for (CSVField& field : row) {
-            std::string  prop = field.get<>();
 
-            float f = ConvStrToFlt(prop);
-            if (f != -FLT_MAX) {
-                sample.factors.push_back(f);
-            }
-            else {
-                int label = classifier.GetID(prop);
+        for (int i = 0; i < row.size(); i++) {
+            std::string  p = row[i].get<>();
+
+            if (i + 1 != row.size()) {
+                sample.factors.push_back(ConvStrToFlt(p));
+            } else {
+                int label = classifier.GetID(p);
                 sample.label = label;
             }
         }
@@ -158,13 +153,47 @@ int main() {
         data_set.push_back(sample);
     }
 
+    // normalize data set
+    int num_factors = data_set.back().factors.size();
+    std::vector<float> mean(num_factors);
+    std::fill(mean.begin(), mean.end(), 0.0f);
+
+    for (const Sample& s : data_set) {
+        for (int i = 0; i < num_factors; i++) {
+            mean[i] += s.factors[i];
+        }
+    }
+
+    for (int i = 0; i < num_factors; i++) {
+        mean[i] /= data_set.size();
+    }
+
+    std::vector<float> stddev(num_factors);
+    std::fill(stddev.begin(), stddev.end(), 0.0f);
+
+    for (const Sample& s : data_set) {
+        for (int i = 0; i < num_factors; i++) {
+            float diff = s.factors[i] - mean[i];
+            stddev[i] += diff * diff;
+        }
+    }
+    
+    for (int i = 0; i < num_factors; i++) {
+        stddev[i] = sqrt(stddev[i] / (data_set.size() - 1));
+    }
+
+    for (Sample& s : data_set) {
+        for (int i = 0; i < num_factors; i++) {
+            s.factors[i] = (s.factors[i] - mean[i]) / stddev[i];
+        }
+    }
+
     // now that we have our data, let's use 50% of it train
-    std::shuffle(data_set.begin(), data_set.end(), std::default_random_engine(std::time(nullptr)));
+    std::shuffle(data_set.begin(), data_set.end(), std::default_random_engine(rand()));
 
     auto select_iter = data_set.begin() + (data_set.size() / 3);
 
     std::vector<Sample> training_set, test_set;
-
     std::copy(data_set.begin(), select_iter, std::back_inserter(training_set));
     std::copy(select_iter, data_set.end(), std::back_inserter(test_set));
 
@@ -172,14 +201,22 @@ int main() {
 
     // test how well our model performs
     int misclassifications = 0;
-
     for (const Sample& sample : test_set) {
         if (classifier.PredictClass(sample) != sample.label) {
             misclassifications++;
         }
     }
 
-    std::cout << "Misclassification rate: " << (float)misclassifications / test_set.size() << '\n';
+    float mcr = (float)misclassifications / test_set.size();
+    std::cout << "Misclassification rate: " << mcr << '\n';
+    return mcr;
+}
 
-
+int main() {
+    int num_tests = 1024;
+    float mcr = 0.0f;
+    for (int i = 0; i < num_tests; i++) {
+        mcr += program();
+    }
+    std::cout << "Overall error " << mcr / num_tests << '\n';
 }
