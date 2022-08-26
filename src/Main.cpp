85cca1f5c8b4d9d8dbef13b4e9cb129ad67041a7
Shadax-stack <58188800+Shadax-stack@users.noreply.github.com>
2022-08-25 18:57:34 -0700

    Multiclassification

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,14 +15,14 @@ float ConvStrToFlt(const std::string& s) {
     std::istringstream iss(s);
     float f;
     iss >> std::noskipws >> f;
-    return (iss.eof() && !iss.fail() ? f : FLT_MIN); // 
+    return (iss.eof() && !iss.fail() ? f : -FLT_MAX); // 
 }
 
 struct Sample {
     std::vector<float> factors;
     int label;
 
-    float get_input(int x) const {
+    float GetInput(int x) const {
         if (x == 0) {
             return 1.0f;
         }
@@ -32,48 +32,127 @@ struct Sample {
     }
 };
 
-int classify(const std::vector<float>& weights, const Sample& sample) {
-    float z = 0.0f;
-    for (int k = 0; k < weights.size(); k++) {
-        z += weights[k] * sample.get_input(k);
+class BinaryClassifier {
+public:
+    virtual float PredictChance(const Sample& sample) = 0; // phi(z) func
+    virtual bool PredictBinary(const Sample& sample) = 0; // binary value
+    virtual int PredictClass(const Sample& sample) = 0; // pos or neg class
+
+    virtual void Train(const std::vector<Sample>& training_set) = 0;
+protected:
+    std::vector<float> weights;
+};
+
+class Perceptron : public BinaryClassifier {
+public:
+    virtual float PredictChance(const Sample& sample) {
+        float z = 0.0f;
+        for (int k = 0; k < weights.size(); k++) {
+            z += weights[k] * sample.GetInput(k);
+        }
+        return z;
     }
-    int classification = (z < 0.0f ? -1 : 1);
-    return classification;
-}
+
+    virtual bool PredictBinary(const Sample& sample) {
+        return (PredictChance(sample) < 0.0f ? false : true);
+    }
+
+    virtual int PredictClass(const Sample& sample) {
+        return  (PredictChance(sample) < 0.0f ? -1 : 1);
+    }
+
+    virtual void Train(const std::vector<Sample>& training_set) {
+        weights.resize(training_set.back().factors.size() + 1);
+        std::fill(weights.begin(), weights.end(), 0.0f);
+
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < training_set.size(); j++) {
+                // compute the result and misclassification error
+                int classification = PredictClass(training_set[j]);
+
+                // compute adjustment
+                for (int k = 0; k < weights.size(); k++) {
+                    weights[k] += learning_rate * (training_set[j].label - classification) * training_set[j].GetInput(k);
+                }
+            }
+        }
+    }
+};
+
+template<class T>
+class MultiClassifier {
+public:
+    MultiClassifier() : num_ids(0) {}
+
+    int GetID(const std::string& s) {
+        int id;
+        auto iter = class_ids.find(s);
+        if (iter == class_ids.end()) {
+            id = num_ids++;
+            class_ids.emplace(s, id);
+        }
+        else {
+            id = iter->second;
+        }
+        return id;
+    }
+
+    void Train(const std::vector<Sample>& training_set) {
+        int num_factors = training_set.back().factors.size();
+        // for each id, create a binary classifier that determines the sample as "part of class x" or "not part of class x"
+        for (int i = 0; i < num_factors; i++) {
+            std::vector<Sample> onevall_set;
+            for (const Sample& s : training_set) {
+                Sample binclass = s;
+                binclass.label = (binclass.label == i ? 1 : -1);
+                onevall_set.push_back(binclass);
+            }
+
+            T binclassifier;
+            binclassifier.Train(onevall_set);
+
+            classifiers.push_back(binclassifier);
+        }
+    }
+
+    int PredictClass(const Sample& s) {
+        float best_chance = -FLT_MAX;
+        int best_class = 0;
+        for (int i = 0; i < num_ids; i++) {
+            float z = classifiers[i].PredictChance(s);
+            if (z > best_chance) {
+                best_chance = z;
+                best_class = i;
+            }
+        }
+        return best_class;
+    }
+private:
+    int num_ids;
+    std::map<std::string, int> class_ids;
+    std::vector<T> classifiers;
+};
 
 int main() {
     CSVReader reader("iris.csv");
 
     std::vector<Sample> data_set;
 
-    std::map<std::string, int> class_ids;
-    int next_id = 1;
-
+    MultiClassifier<Perceptron> classifier;
 
     for (CSVRow& row : reader) {
         Sample sample;
-        bool binary = true;
         for (CSVField& field : row) {
             std::string  prop = field.get<>();
+
             float f = ConvStrToFlt(prop);
-            if (f != FLT_MIN) {
+            if (f != -FLT_MAX) {
                 sample.factors.push_back(f);
             }
             else {
-                int& label = class_ids[prop];
-                if (label == 0) {
-                    label = next_id++;
-                }
-                if (label == 3) {
-                    binary = false;
-                    break;
-                }
-                sample.label = (label == 1 ? -1 : 1);
+                int label = classifier.GetID(prop);
+                sample.label = label;
             }
-        }
-
-        if (!binary) {
-            continue;
         }
 
         data_set.push_back(sample);
@@ -82,42 +161,20 @@ int main() {
     // now that we have our data, let's use 50% of it train
     std::shuffle(data_set.begin(), data_set.end(), std::default_random_engine(std::time(nullptr)));
 
-    auto select_iter = data_set.begin() + (data_set.size() / 2);
+    auto select_iter = data_set.begin() + (data_set.size() / 3);
 
     std::vector<Sample> training_set, test_set;
 
     std::copy(data_set.begin(), select_iter, std::back_inserter(training_set));
     std::copy(select_iter, data_set.end(), std::back_inserter(test_set));
 
-    // train a perceptron
-
-    // create our weights
-    std::vector<float> weights(data_set.front().factors.size() + 1);
-    for (float f : weights) {
-        f = 0.0f; // init to zero
-    }
-    
-
-    // adjust the weights
-    for (int i = 0; i < 1000; i++) {
-        for (int j = 0; j < training_set.size(); j++) {
-            // compute the result and misclassification error
-            int classification = classify(weights, training_set[j]);
-
-            // compute adjustment
-            for (int k = 0; k < weights.size(); k++) {
-                weights[k] += learning_rate * (training_set[j].label - classification) * training_set[j].get_input(k);
-            }
-        }
-    }
+    classifier.Train(training_set);
 
     // test how well our model performs
-
-
     int misclassifications = 0;
 
     for (const Sample& sample : test_set) {
-        if (classify(weights, sample) != sample.label) {
+        if (classifier.PredictClass(sample) != sample.label) {
             misclassifications++;
         }
     }
